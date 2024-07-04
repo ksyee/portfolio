1688d96f5a8bafa6eac6f1e2cadb42fa1c7b8a54
ksyee <tjssud0647@gmail.com>
2024-07-04 16:50:11 +0900

    [docs] util/supabase.ts에서 사용하지 않는 import 제거
    - util/supabase.ts에서 사용하지 않는 import 제거

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,11 +1,8 @@
-import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-
-
