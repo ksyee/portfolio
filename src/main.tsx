0d4728c8abcd1b3da9fdb52ef63039600c4c63a6
ksy <tjssud0647@gmail.com>
2025-11-20 23:23:49 +0900

    feat: Hero 배경 애니메이션 재구성

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Analytics } from "@vercel/analytics/react";
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Analytics />
-  </>
-);
+  createRoot(document.getElementById("root")!).render(<App />);
   
